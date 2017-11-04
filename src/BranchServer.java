e5410bd6eace918211958f8b7e86945cd1be3913
Sarang <sarangsawant10@gmail.com>
2017-11-03 23:44:39 -0400

    All branches initialized with initial amount

--- src/BranchServer.java
+++ src/BranchServer.java
@@ -1,21 +1,138 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BranchServer {
 
+	int branchBalance=0;	
+	static String branchIPAddress;
+	static String branchName;
+	static int branchPort;
+	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		if(args.length != 2){
 			System.out.println("please provide a valid branch-name and port");
 			System.exit(0);
 		}
 		try {
-			String ipAddress = InetAddress.getLocalHost().getHostAddress();
-			String name = args[0];
-			int port = Integer.valueOf(args[1]);
-			System.out.println("Branch server started " + name + " " + port);
+			branchIPAddress = InetAddress.getLocalHost().getHostAddress();
+			branchName = args[0];
+			branchPort = Integer.valueOf(args[1]);
+			
+			ServerSocket serverSocket = null;
+			try {
+				serverSocket = new ServerSocket(branchPort);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Branch server started " + branchIPAddress + " " + branchPort);
+			BranchServer branchServer = new BranchServer();
+			Bank.BranchMessage branchMsg = null;
+			InputStream inputStream;
+			DataInputStream in; 
+			ObjectInputStream objectInputStream = null;
+			List<String> list = new ArrayList<>();
+			while(true) {
+				try {
+					System.out.println("Listining on port " + branchPort);
+					Socket socket = serverSocket.accept();
+					
+					try {
+						inputStream = socket.getInputStream();
+						
+						if(branchMsg == null) {
+							branchMsg = Bank.BranchMessage.parseFrom(inputStream);
+							
+							if(branchMsg.hasInitBranch())
+								branchServer.initializeBranchDetails(branchMsg);
+						
+						//if(branchMsg.hasTransfer()) {
+							//new Thread
+						//}
+						}
+						else {
+							objectInputStream = new ObjectInputStream(inputStream);
+							list = (List<String>) objectInputStream.readObject();
+							System.out.println("list " + list.toString());
+						}
+						
+						 
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+						
+					//new Thread(new BranchServerHandler(socket, branchIPAddress, branchName , branchPort)).start();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	private void initializeBranchDetails(Bank.BranchMessage branchMsg) {
+		
+		branchBalance = branchMsg.getInitBranch().getBalance();
+		
+		System.out.println("Branch initial amount " + branchBalance);
+		
+		for(Bank.InitBranch.Branch branch : branchMsg.getInitBranch().getAllBranchesList()) {
+			System.out.println(branch.getName() + " " + branch.getIp() + " " + branch.getPort());
+		}
+	}
+	
+	/*private void transferMoneyToAllBranches() {
+		
+		for(Bank.InitBranch.Branch branch : branchMsg.getInitBranch().getAllBranchesList()) {
+			
+			//Exclude itself before making tcp connection to all branches
+			if(! (branch.getName().equals(branchName) && branch.getIp().equals(branchIPAddress) && (branch.getPort()==branchPort)) ) {
+				Socket clientSocket;
+				try {
+					clientSocket = new Socket(branch.getIp(), branch.getPort());
+					Bank.Transfer.Builder transferMsgBuilder = Bank.Transfer.newBuilder();
+					transferMsgBuilder.setMoney(10);
+					
+					Bank.BranchMessage.Builder branchMsgBuilder  = Bank.BranchMessage.newBuilder();
+					branchMsgBuilder.setTransfer(transferMsgBuilder);
+					
+					OutputStream outputStream = clientSocket.getOutputStream();
+					branchMsgBuilder.build().writeTo(outputStream);
+					
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	*/
+	private synchronized void updateBalance(int amount){
+		branchBalance = branchBalance + amount;
+	}
+	
+	
+		
+
 }
--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,20 @@
-
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
+	
 	public static void main(String[] args) {
 		if(args.length != 2){
 			System.out.println("Invalid number of arguments");
@@ -10,11 +23,105 @@ public class Controller {
 		try {
 			int totalAmount = Integer.valueOf(args[0]);
 			String fileName = args[1];
-			System.out.println("Controller started " + totalAmount + " " + fileName);
+			Controller controller = new Controller();
+			controller.parseFileAndInitializeBranches(totalAmount, fileName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+	
+	private void parseFileAndInitializeBranches(int amount, String fileName) {
+		File file = new File(fileName);
+		BufferedReader bufferedReader = null;
+		if(!file.exists()) {
+			System.out.println("Cannot open file " + fileName + "!!");
+			System.exit(0);
+		}
+		
+		try {
+			bufferedReader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.print("File not found..!");
+			System.exit(0);
+		}
+		
+		int branchCount = 0;
+		String line = "";
+		Bank.InitBranch.Builder branchBuilder = Bank.InitBranch.newBuilder();
+		try {
+			while((line = bufferedReader.readLine()) != null){
+				String arr[] = line.split(" ");
+				String branchName = arr[0];
+				String ipAddress = arr[1];
+				int port = Integer.parseInt(arr[2]);
+				
+				System.out.println(branchName + " " + ipAddress + " " + port);
+				Bank.InitBranch.Branch.Builder branch = Bank.InitBranch.Branch.newBuilder();
+				branch.setName(branchName);
+				branch.setIp(ipAddress);
+				branch.setPort(port);
+				
+				branchBuilder.addAllBranches(branch);
+				branchCount++;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int branchInitAmt = amount/branchCount;
+		System.out.println("Initial balance for each branch is "+ branchInitAmt);
+		branchBuilder.setBalance(branchInitAmt);
+
+		Bank.BranchMessage.Builder branchMsgBuilder  = Bank.BranchMessage.newBuilder();
+		branchMsgBuilder.setInitBranch(branchBuilder);
+		
+		
+		for(Bank.InitBranch.Branch branch : branchBuilder.getAllBranchesList()) {
+			Socket clientSocket;
+			try {
+				System.out.println("connecting to " + branch.getIp() +  branch.getPort());
+				clientSocket = new Socket(branch.getIp(), branch.getPort());
+				OutputStream outputStream = clientSocket.getOutputStream();
+				branchMsgBuilder.build().writeTo(outputStream);
+							
+				clientSocket.close();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		List<String> list = new ArrayList<>();
+		
+		for(Bank.InitBranch.Branch branch : branchBuilder.getAllBranchesList()) {
+			Socket clientSocket;
+			try {
+				System.out.println("connecting to " + branch.getIp() +  branch.getPort());
+				clientSocket = new Socket(branch.getIp(), branch.getPort());
+				OutputStream outputStream = clientSocket.getOutputStream();
+				
+				ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+				objectOutputStream.writeObject(list);
+				
+				list.add(branch.getName());
+				clientSocket.close();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
 	}
 
 }
